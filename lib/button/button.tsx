5101553fe5a19351a01638c693b53d576d6034a4
周字彪 <1247821135@qq.com>
2022-03-17 16:08:24 +0800

    抽离 buttonClassName.tsx

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './button.scss';
+import buttonClassName from '../helpers/buttonClassName';
 
-type Props = {
+export type Props = {
   theme?: 'button' | 'link' | 'text',
   size?: 'big' | 'small' | 'normal',
   level?: 'main' | 'normal' | 'danger',
@@ -9,38 +10,9 @@ type Props = {
   loading?: boolean
 }
 const Button: React.FC<Props> = (props) => {
-  const {theme, size, level,loading} = props;
-  const className = () => {
-    const classNameList: string[] = [];
-    if (theme === 'text') {
-      classNameList.push('star-theme-text');
-    } else if (theme === 'link') {
-      classNameList.push('star-theme-link');
-    } else {
-      classNameList.push('star-theme-button');
-    }
-    if (size === 'small') {
-      classNameList.push('star-size-small');
-    } else if (size === 'big') {
-      classNameList.push('star-size-big');
-    } else {
-      classNameList.push('star-size-normal');
-    }
-    if (level === 'main') {
-      classNameList.push('star-level-main');
-    } else if (level === 'danger') {
-      classNameList.push('star-level-danger');
-    } else {
-      classNameList.push('star-level-normal');
-    }
-    if (loading){
-      classNameList.push('star-loading')
-    }
-    return classNameList.join(' ');
-  };
   return (
-    <button className={'star-button ' + className()} disabled={props.disabled}>
-      {loading?<span className="star-loadingIndicator"/>:null}
+    <button className={'star-button ' + buttonClassName(props)} disabled={props.disabled}>
+      {props.loading?<span className="star-loadingIndicator"/>:null}
       {props.children}
     </button>
   );
---
+++ lib/helpers/buttonClassName.tsx
@@ -0,0 +1,32 @@
+import {Props} from '../button/button';
+
+const buttonClassName = (props:Props)=>{
+  const {theme, size, level,loading} = props;
+  const classNameList: string[] = [];
+  if (theme === 'text') {
+    classNameList.push('star-theme-text');
+  } else if (theme === 'link') {
+    classNameList.push('star-theme-link');
+  } else {
+    classNameList.push('star-theme-button');
+  }
+  if (size === 'small') {
+    classNameList.push('star-size-small');
+  } else if (size === 'big') {
+    classNameList.push('star-size-big');
+  } else {
+    classNameList.push('star-size-normal');
+  }
+  if (level === 'main') {
+    classNameList.push('star-level-main');
+  } else if (level === 'danger') {
+    classNameList.push('star-level-danger');
+  } else {
+    classNameList.push('star-level-normal');
+  }
+  if (loading){
+    classNameList.push('star-loading')
+  }
+  return classNameList.join(' ');
+}
+export default buttonClassName
