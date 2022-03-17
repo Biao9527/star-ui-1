25a6f7c6e70f46b4869587247dd470c40b638f63
周字彪 <1247821135@qq.com>
2022-03-17 16:01:34 +0800

    完成 Button 组件

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React from 'react';
+import Button from './button';
 
-const buttonExample = ()=>{
+const buttonExample = () => {
   return (
-    <div> button </div>
-  )
-}
-export default buttonExample
+    <div>
+      <h2>Button</h2>
+      <div>
+        <Button>button</Button>
+        <Button theme="button">button</Button>
+        <Button theme="link">button</Button>
+        <Button theme="text">button</Button>
+      </div>
+      <h2>Size</h2>
+      <div>
+        <Button size="big">button</Button>
+        <Button>button</Button>
+        <Button size="small">button</Button>
+      </div>
+      <div>
+        <Button theme="link" size="big">button</Button>
+        <Button theme="link" size="normal">button</Button>
+        <Button theme="link" size="small">button</Button>
+      </div>
+      <div>
+        <Button theme="text" size="big">button</Button>
+        <Button theme="text" size="normal">button</Button>
+        <Button theme="text" size="small">button</Button>
+      </div>
+      <h2>Level</h2>
+      <div>
+        <Button level="main">button</Button>
+        <Button>button</Button>
+        <Button level="danger">button</Button>
+      </div>
+      <div>
+        <Button theme="link" level="main">button</Button>
+        <Button theme="link" level="normal">button</Button>
+        <Button theme="link" level="danger">button</Button>
+      </div>
+      <div>
+        <Button theme="text" level="main">button</Button>
+        <Button theme="text" level="normal">button</Button>
+        <Button theme="text" level="danger">button</Button>
+      </div>
+      <h2>Disabled</h2>
+      <div>
+        <div>
+          <Button disabled>button</Button>
+          <Button>button</Button>
+        </div>
+        <div>
+          <Button disabled theme="link">button</Button>
+          <Button theme="link">button</Button>
+        </div>
+        <div>
+          <Button disabled theme="text">button</Button>
+          <Button theme="text">button</Button>
+        </div>
+      </div>
+      <h2>Loading</h2>
+      <div>
+        <Button loading>loading</Button>
+        <br/>
+        <Button>finish</Button>
+      </div>
+    </div>
+  );
+};
+export default buttonExample;
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import './button.scss';
 
-const Button:React.FC = ()=>{
-  return(
-    <button>button</button>
-  )
+type Props = {
+  theme?: 'button' | 'link' | 'text',
+  size?: 'big' | 'small' | 'normal',
+  level?: 'main' | 'normal' | 'danger',
+  disabled?: boolean,
+  loading?: boolean
 }
+const Button: React.FC<Props> = (props) => {
+  const {theme, size, level,loading} = props;
+  const className = () => {
+    const classNameList: string[] = [];
+    if (theme === 'text') {
+      classNameList.push('star-theme-text');
+    } else if (theme === 'link') {
+      classNameList.push('star-theme-link');
+    } else {
+      classNameList.push('star-theme-button');
+    }
+    if (size === 'small') {
+      classNameList.push('star-size-small');
+    } else if (size === 'big') {
+      classNameList.push('star-size-big');
+    } else {
+      classNameList.push('star-size-normal');
+    }
+    if (level === 'main') {
+      classNameList.push('star-level-main');
+    } else if (level === 'danger') {
+      classNameList.push('star-level-danger');
+    } else {
+      classNameList.push('star-level-normal');
+    }
+    if (loading){
+      classNameList.push('star-loading')
+    }
+    return classNameList.join(' ');
+  };
+  return (
+    <button className={'star-button ' + className()} disabled={props.disabled}>
+      {loading?<span className="star-loadingIndicator"/>:null}
+      {props.children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
