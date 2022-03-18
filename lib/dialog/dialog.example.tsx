7464d36e4d707496b2a401b78659ab4029a103df
周字彪 <1247821135@qq.com>
2022-03-18 13:12:10 +0800

    完成 alert 和 dialog

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,14 +1,33 @@
 import React, {useState} from 'react';
-import Dialog from './dialog';
+import Dialog, {alert} from './dialog';
+import Button from '../button/button';
 
 const dialogExample: React.FC = () => {
-  const [x,setX] = useState(false)
+  const [x, setX] = useState(false);
+  const fn = () => {
+    console.log('ok');
+    return false;
+  };
+  const fn1 = () => {
+    console.log('cancel');
+    return true;
+  };
   return (
     <div>
-      <button onClick={()=>setX(!x)}>dialog</button>
-      <Dialog visible={x}>
-        <div>hi</div>
-      </Dialog>
+      <div>
+        <Button onClick={() => setX(!x)}>dialog</Button>
+        <Dialog visible={x}
+                title="我是个标题"
+                onClose={(bool) => setX(bool)}
+                ok={fn}
+                cancel={fn1}>
+          <div>hi</div>
+          <div>ho1</div>
+        </Dialog>
+      </div>
+      <div>
+        <Button onClick={()=>alert('hello','提示')}>alert</Button>
+      </div>
     </div>
   );
 };
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import './dialog.scss'
+import './dialog.scss';
+import Button from '../button/button';
+import ReactDOM from 'react-dom';
+import Dialog from './dialog';
 
 type Props = {
-  visible: boolean
+  title: string,
+  visible: boolean,
+  onClose: (bool: boolean) => void,
+  closeOnClickOverlay?: boolean,
+  ok?: () => void,
+  cancel?: () => void,
+  alert?: boolean
 }
+
 const dialog: React.FC<Props> = (props) => {
-  return (
+  const closeOverlay = () => {
+    if (props.closeOnClickOverlay) {
+      props.onClose(false);
+    }
+  };
+  const ok = () => {
+    if (props.ok&&props.ok()) {
+      props.onClose(false);
+    }
+  };
+  const cancel = () => {
+    if (props.cancel&&props.cancel()) {
+      props.onClose(false);
+    }
+  };
+  const dialogDiv =
     props.visible ?
       <>
-        <div className="star-dialog-mask"/>
-        <div className="star-dialog">
-          <header>标签</header>
-          <main>{props.children}</main>
-          <footer>
-            <button>ok</button>
-            <button>cancel</button>
-          </footer>
-         </div>
+        <div className="star-dialog-overlay" onClick={closeOverlay}/>
+        <div className="star-dialog-wrapper">
+          <div className="star-dialog">
+            <header>
+              {props.title}
+              <span onClick={() => props.onClose(false)} className="star-dialog-close"/>
+            </header>
+            <main>
+              {props.children}
+            </main>
+            <footer>
+              {props.alert ? null :
+                <>
+                  <Button level="main" onClick={ok}>OK</Button>
+                  <Button onClick={cancel}>Cancel</Button>
+                </>
+              }
+            </footer>
+          </div>
+        </div>
       </> :
-      null
-  );
+      null;
+  return ReactDOM.createPortal(dialogDiv, document.body);
 };
+
+const alert = (content: string, title: string)=>{
+  const component = <Dialog
+    alert
+    visible={true} title={title}
+    onClose={()=>{
+      ReactDOM.render(React.cloneElement(component,{visible:false}),div)
+      ReactDOM.unmountComponentAtNode(div)
+      div.remove()
+    }}>{content}</Dialog>;
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(component,div)
+}
+
+export {alert}
 export default dialog;
