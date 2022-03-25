e4d8ed590949d1e9f3bd80fd7dfdabb424b6e0de
周字彪 <1247821135@qq.com>
2022-03-25 14:37:03 +0800

    Switch 支持visible，size，disabled属性

--- lib/switch/switch.example.tsx
+++ lib/switch/switch.example.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Switch from './switch';
 
 const SwitchExample: React.FC = () => {
+  const [visible,setVisible] = useState(false)
   return (
     <div>
         <h2>例子一</h2>
-      <Switch/>
+      <Switch disabled className='xxx' size='big' visible={visible} onClick={()=>setVisible(!visible)}/>
     </div>
-
   );
 };
 
--- lib/switch/switch.tsx
+++ lib/switch/switch.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import './switch.scss'
+import classes from '../helpers/classes';
 
-const Switch = ()=>{
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+  visible: boolean
+  size?: 'big' | 'small' | 'normal'
+  disabled?: boolean
+}
+const Switch:React.FC<Props> = (props)=>{
+  const {visible,size,disabled,className,...rest} = props
+  const name = ()=>{
+    const classNameList = []
+    if (visible){
+      classNameList.push('star-checked')
+    }
+    if (size === 'big'){
+      classNameList.push('star-switch-big')
+    }else if (size === 'small'){
+      classNameList.push('star-switch-small')
+    }
+    if (disabled){
+      classNameList.push('star-switch-disabled')
+    }
+    return classNameList.join(' ')
+  }
   return (
-    <div>
-      <button className="star-switch">
+      <button
+        className={classes('star-switch',name(),className)}
+        disabled={disabled}
+        {...rest}>
       <span/>
-    </button>
-    </div>
+      </button>
   )
 }
 
