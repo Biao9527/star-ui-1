f619c8ae62e4818cc9a5200503fdc6e2b623f66d
周字彪 <1247821135@qq.com>
2022-03-25 13:54:42 +0800

    添加switch组件

---
+++ lib/switch/switch.example.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Switch from './switch';
+
+const SwitchExample: React.FC = () => {
+  return (
+    <div>
+        <h2>例子一</h2>
+      <Switch/>
+    </div>
+
+  );
+};
+
+export default SwitchExample;
---
+++ lib/switch/switch.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './switch.scss'
+
+const Switch = ()=>{
+  return (
+    <div>
+      <button className="star-switch">
+      <span/>
+    </button>
+    </div>
+  )
+}
+
+export default Switch
