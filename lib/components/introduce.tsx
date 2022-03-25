084bbcecf9ffeb692083c30b2e89d1b89694f15a
周字彪 <1247821135@qq.com>
2022-03-25 13:56:23 +0800

    完成Switch样式

--- lib/components/introduce.tsx
+++ lib/components/introduce.tsx
@@ -4,8 +4,8 @@ const Introduce = () => {
   return (
     <div className='markdown-body'>
       <h2>介绍</h2>
-      <p>Star UI 是一款基于 React 和 TypeScript 的 UI 组件库。</p>
-      <p>这这款组件库是本人学习Vue3所编写 UI 库，全程亲手编写，尽量不采用第三方库。</p>
+      <p>Star UI 是一款基于 React + TypeScript 的 UI 组件库。</p>
+      <p>这这款组件库是本人学习React所编写 UI 库，全程亲手编写，尽量不采用第三方库。</p>
       <p>历史提交信息非常规范，你可以按提交的顺序逐个查看，你也可以直接查看每个组件的源代码和示例，运行方法见 README.md</p>
       <p>本组件库仅供学习交流，请勿在生产环境中使用。</p>
       下一节：<a href="#/install">安装</a>
